4d34115baf663bbf0b3f7b742f4a4d4c7ed36a43
Piotr Bosak <pbk@touk.pl>
2022-05-28 18:34:28 +0200

    change stuff

--- src/logic/game.rs
+++ src/logic/game.rs
@@ -1,27 +1,42 @@
 use crate::{Board, Move, Position};
 
-//lepiej, żeby frontend nie używal boarda bezposrenio tylko zawsze posrednio przez game
-//a current board i history bylo prywatne
 pub struct Game {
     current_board: Board,
     history: Vec<Board>,
     turn: Turn,
 }
 
-//todo warto dodac funkcje ktora pokaze faktycznie wszystkie mozliwe ruchy
-//tzn ze jezeli niby jest mozliwy ale wtedy by odslonil króla to jednak nie można
-//moze w sumie warto zeby to zwracalo nową wersje gry, nawet jezeli to bedzie moved
-//wtedy zwroci sie Option<Game> i bedzie wszystko jasne
 impl Game {
+    pub fn new() -> Game {
+        Game {
+            current_board: Board::new(),
+            history: vec![],
+            turn: Turn::White,
+        }
+    }
     pub fn possible_moves(&self, position: &Position) -> Vec<Move> {
         self.current_board.possible_valid_moves(position)
     }
+    pub fn make_move(&mut self, from: &Position, to: &Position) -> Result<(), &str> {
+        match self.current_board.make_move(from, to) {
+            Some(board) => {
+                self.update_game(board);
+                Ok(())
+            }
+            None => Err("invalid move")
+        }
+    }
 
-
-    // pub fn make_move(&mut self, from: &Position, to: &Position) {
-    //     let possible_moves = &self.current_board.possible_moves(from);
-    // }
+    fn update_game(&mut self, new_board: Board) {
+        self.history.push(self.current_board.clone());
+        self.current_board = new_board;
+        self.turn = match self.turn {
+            Turn::White => Turn::Black,
+            Turn::Black => Turn::White,
+        };
+    }
 }
+
 
 pub enum Turn {
     White,
